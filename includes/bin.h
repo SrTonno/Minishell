72450e5220c4601be36bb84613eeb2722d4246a6
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-03-28 20:15:43 +0200

    create env

---
+++ includes/bin.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bin.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 20:13:54 by tvillare          #+#    #+#             */
+/*   Updated: 2023/03/28 20:13:58 by tvillare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BIN_H
+# define BIN_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+# include "libft.h"
+
+typedef struct s_env
+{
+	char	*var;
+	char	*data;
+}	t_env;
+
+
+#endif
