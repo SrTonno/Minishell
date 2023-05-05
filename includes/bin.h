d3376fa2addd2e44f02e553a2b7261ff9c235ead
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-05-05 13:32:54 +0200

    compila

--- includes/bin.h
+++ includes/bin.h
@@ -6,7 +6,7 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 20:13:54 by tvillare          #+#    #+#             */
-/*   Updated: 2023/05/05 13:26:52 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/05/05 13:32:45 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ int		ft_unset(char ***env, char **command);
 
 int		isBuiltin(char *command);
 int		isNoChildBuiltin(char *command);
+
 
 #endif
